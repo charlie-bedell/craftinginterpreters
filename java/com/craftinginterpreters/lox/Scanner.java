45453353b15e657987f3ac6d1cbdf695ad0daf68
Charles Bedell <charlesbedell5@gmail.com>
2024-07-03 12:33:59 -0400

    bugfix: add breaks to prevent fallthroughs creating wrong tokens

--- java/com/craftinginterpreters/lox/Scanner.java
+++ java/com/craftinginterpreters/lox/Scanner.java
@@ -72,12 +72,16 @@ class Scanner {
 				case '*': addToken(STAR); break;
 				case '!':
 						addToken(match('=') ? BANG_EQUAL : BANG);
+						break;
 				case '=':
 						addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+						break;
 				case '<':
 						addToken(match('=') ? LESS_EQUAL : LESS);
+						break;
 				case '>':
 						addToken(match('=') ? GREATER_EQUAL : GREATER);
+						break;
 				case '/':
 						if (match('/')) {
 								// A comment goes until the end of the line.
@@ -88,9 +92,8 @@ class Scanner {
 								addToken(SLASH);
 						}
 						break;
-
-				case ' ':
-				case '\t':
+				case ' ':  // fall through
+				case '\t': // fall through
 				case '\r':
 						break;
 				case '\n':
